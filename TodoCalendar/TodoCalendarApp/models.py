746078ccbe6a563fc2c1c2f93f167bd84727387b
yunhl <yunhl@student.unimelb.edu.au>
2023-12-04 00:46:59 +1100

    added User model

--- TodoCalendar/TodoCalendarApp/models.py
+++ TodoCalendar/TodoCalendarApp/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.conf import settings
+
+# Used to create users and superusers for the database
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+# The user class, used for login and register
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True, # a primary key
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_researcher = models.BooleanField(default=False)
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
