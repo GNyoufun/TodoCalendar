77003dffcc05e0c44d52d40c504410734832ff16
yunhl <yunhl@student.unimelb.edu.au>
2023-12-08 00:48:17 +1100

    Created Todo logging model, forms, views, html, databases and add the url path

--- TodoCalendar/TodoCalendarApp/models.py
+++ TodoCalendar/TodoCalendarApp/models.py
@@ -1,54 +1,17 @@
 from django.db import models
 
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.conf import settings
 
-# Used to create users and superusers for the database
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        if not email:
-            raise ValueError("Users must have an email address")
-        user = self.model(
-            email=self.normalize_email(email),
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+class Labels(models.Model):
+    labels = models.CharField(max_length=30)
 
-    def create_superuser(self, email, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
+class todo(models.Model):
+    taskName = models.CharField(max_length=30)
+    taskDescription = models.TextField()
+    openDate = models.DateTimeField(auto_now_add=True)
+    dueDate = models.DateTimeField(blank=False, null=False)
+    completed = models.BooleanField(default=False)
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE) #! will add in later day
+    label = models.ManyToManyField(Labels)
 
-# The user class, used for login and register
-class User(AbstractBaseUser):
-    email = models.EmailField(
-        verbose_name="email address",
-        max_length=255,
-        unique=True, # a primary key
-    )
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_researcher = models.BooleanField(default=False)
-    objects = UserManager()
-    
-    USERNAME_FIELD = "email"
 
-    def __str__(self):
-        return self.email
-    
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return self.is_admin
