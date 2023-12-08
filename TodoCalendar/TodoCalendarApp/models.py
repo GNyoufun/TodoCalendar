ebe6553a4441fe2dca6ee731279b732e3142ed5a
yunhl <yunhl@student.unimelb.edu.au>
2023-12-08 11:37:47 +1100

    added comment for models

--- TodoCalendar/TodoCalendarApp/models.py
+++ TodoCalendar/TodoCalendarApp/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 from django.conf import settings
 
+# User created label for todo items 
 class Labels(models.Model):
     labels = models.CharField(max_length=30)
 
+# DB for the todo items
 class todo(models.Model):
     taskName = models.CharField(max_length=30)
     taskDescription = models.TextField()
