3f5a5a139774decaecf0ea600dfba47f979d5de6
yunhl <yunhl@student.unimelb.edu.au>
2023-12-18 10:25:06 +1000

    add folder for division and basic home page html and css

--- TodoCalendar/TodoCalendar/settings.py
+++ TodoCalendar/TodoCalendar/settings.py
@@ -10,11 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(BASE_DIR)
+
+STATIC_URL = 'TodoCalendarApp/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'TodoCalendarApp/static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
