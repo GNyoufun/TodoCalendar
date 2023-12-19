53cfc433237a5e6683cad0a30f3048d54324768d
yunhl <yunhl@student.unimelb.edu.au>
2023-12-20 01:27:49 +1000

    update the html and css for home page and added year/month to the url and UI

--- TodoCalendar/TodoCalendarApp/urls.py
+++ TodoCalendar/TodoCalendarApp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("<int:year>/", views.home, name="home"),
+    path("<int:year>/<int:month>/", views.home, name="home"),
     path("todoAdd", views.todoAddOn, name="todoAddOn")
 ]
--- TodoCalendar/TodoCalendarApp/views.py
+++ TodoCalendar/TodoCalendarApp/views.py
@@ -1,11 +1,25 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import messages
 
 from . import forms
+
+from datetime import date
+
+import calendar 
+
+
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def home(request, year=date.today().year, month = date.today().month):
+    if month > 12 or month < 1:
+        messages.warning(request, "The month entered are invalid, please enter valid month.")
+        curMonth = date.today().month
+
+        return render(request,'home.html', {"year": year, "month": curMonth})
+    month = list(calendar.month_name)[month]
+
+    return render(request, 'home.html', {"year": year, "month": month})
 
 def todoAddOn(request):
     if request.method == 'POST':
